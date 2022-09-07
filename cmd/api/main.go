a5b8f8c42d9a5c9817e71391c3b035c0e572dbfc
Francesco <francesco.varsallona@gmail.com>
2022-09-07 19:05:44 +0200

    Open DB connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 type config struct {
@@ -27,6 +31,14 @@ func main() {
 		},
 	}
 
+	db, err := openDB()
+
+	if err != nil {
+		app.logger.Fatal(err)
+		return
+	}
+	defer db.Close()
+
 	srv := &http.Server{
 		Handler: app.router(),
 		Addr:    fmt.Sprintf("127.0.0.1:%d", app.config.port),
@@ -37,9 +49,31 @@ func main() {
 
 	log.Printf("Server is running on port: %d", app.config.port)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 	}
 
+}
+
+func openDB() (*sql.DB, error) {
+	pwd := os.Getenv("DB_PASSWORD")
+
+	if pwd == "" {
+		return nil, errors.New("no password for the database was provided")
+	}
+
+	db, err := sql.Open("postgres", fmt.Sprintf("postgres://francesco:%s@localhost/homeManager?sslmode=disable", pwd))
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
